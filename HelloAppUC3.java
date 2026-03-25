b28f28260ea6ed979c9aec4f4829d0a3690cc561
yugalrathod <yugalrathod3388@gmail.com>
2026-03-25 08:39:25 +0530

    UC3 done

---
+++ HelloAppUC3.java
@@ -0,0 +1,14 @@
+public class HelloAppUC3 {
+    public static void main(String[] args) {
+       
+        String name = "World";
+
+     
+        if (args.length > 0) {
+            name = args[0]; 
+        }
+
+       
+        System.out.println("Hello, " + name + "!");
+    }
+}
--- HelloAppuc2.java
+++
@@ -1,8 +0,0 @@
-public class HelloAppuc2 {
-    public static void main(String[] args) {
-        
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        
-    }
-}
