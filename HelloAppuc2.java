6eecbeff22caaa2e18645e2649f3a37d0ff07d27
yugalrathod <yugalrathod3388@gmail.com>
2026-03-25 08:15:35 +0530

    uc2 done

---
+++ HelloAppuc2.java
@@ -0,0 +1,8 @@
+public class HelloAppuc2 {
+    public static void main(String[] args) {
+        
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        
+    }
+}
